4a4747e6702ba58a8043f59fa62fa88231594067
huahua <huahua@huahua.local>
2016-07-06 11:42:05 +0800

    Initial Commit

---
+++ WuDou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WuDou
+//
+//  Created by huahua on 16/7/6.
+//  Copyright © 2016年 os1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
